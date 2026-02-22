ef3175d63082fa7df4f1ac64f71e4d53d8d27dbd
redamohsen14-gif <reda.mohsen14@gmail.com>
2026-02-22 14:12:11 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_GXcHiAro6mY2e50tqhgBsLjE9BfXe18",
+  apiKey: "AIzaSyA_GXcHiAro6mV2e50tqhgBsLje9BFxe18",
   authDomain: "alshalawi-reda.firebaseapp.com",
   projectId: "alshalawi-reda",
-  storageBucket: "alshalawi-reda.firebasestorage.app",
-  messagingSenderId: "862676021965",
-  appId: "1:862676021965:web:7fc40763bdb73dcaa42f84",
+  storageBucket: "alshalawi-reda.appspot.com",
+  messagingSenderId: "862670621965",
+  appId: "1:862670621965:web:7fc40763bdb73dcaa42f84",
   measurementId: "G-FB4K0DYMNY"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
